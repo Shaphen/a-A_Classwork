ff299908f170085cea342371626b267b7eb9902c
Shaphen <swpburn@gmail.com>
2020-04-08 15:57:55 -0700

    Update to TTT with jquery up to congratulatory message

--- Week_Nine/D3/tictactoe_project/src/index.js
+++ Week_Nine/D3/tictactoe_project/src/index.js
@@ -1,6 +1,10 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view.js");
+const Game = require("./game.js");
 
-  $(() => {
-    // Your code here
+  $(() => { //only runs when all HTML loads
+    let game = new Game;
+    let $figure = $(".ttt");
+    let board = new View(game, $figure)
+    board.setupBoard();
+    board.bindEvents();
   });
--- Week_Nine/D3/tictactoe_project/src/ttt-view.js
+++ Week_Nine/D3/tictactoe_project/src/ttt-view.js
@@ -1,11 +1,84 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+  }
 
-  bindEvents() {}
+  bindEvents() {
+    let $li = $('li')
+    let that = this
+    $li.each(function(index) {
+      $(this).on("click", () => {
+        console.log($(this).data("pos"));
+        // alert("it works")
+        that.game.playMove($(this).data("pos"))
+        that.makeMove($(this));
+      
+      })
+    });
 
-  makeMove($square) {}
+  }
 
-  setupBoard() {}
+  makeMove($square) {
+    let pos = $square.data("pos");
+    let mark = this.game.board.grid[pos[0]][pos[1]];
+    if(mark === "x"){
+      $square.removeClass("initial");
+      $square.addClass("x");
+    } else {
+      $square.removeClass("initial");
+      $square.addClass("o");
+    }
+    $square.text(mark);
+
+    if (this.game.isOver()) {
+      let $p = $('<h1></h1>');
+      $p.addClass("congrats");
+      $p.text(`Congratlations ${mark}. You win`);
+      $('ul').append($p);
+    }
+
+  }
+
+
+
+
+  setupBoard() {
+    //make blank HTMl ele
+    let $ul = $("<ul></ul>");
+    //add a classname to ele
+    $ul.addClass("board");
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0 ; j < 3; j++) {
+      let $li = $("<li></li>");
+      $li.addClass("square");
+      $li.addClass("initial");
+      $li.data("pos", [i,j] );
+      //append sticks arg onto thing invoking it
+      $ul.append($li);
+    }
+  }
+
+  //appending it onto something in HTML file
+  this.$el.append($ul);
+  }
 }
+
+
+
+// View.prototype.setupBoard = function(){
+//   let $ul = $("<ul></ul>");
+//   $ul.addClass("board");
+
+//   for (let i = 0; i < 9; i++) {
+//     let $li = $("<li></li>");
+//     $li.addClass("square");
+//     $ul.append($li);
+//   }
+
+//   let $figure = $("figure");
+//   $figure.append($ul);
+// }
 
 module.exports = View;
