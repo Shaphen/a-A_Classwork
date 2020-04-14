b3b0830e4b6d42beeb6a7a1d5a241bccccbb495a
Shaphen <swpburn@gmail.com>
2020-04-14 15:53:01 -0700

    Add clock feature to widgets project W10D2

---
+++ Week_Ten/D2/widgets/frontend/clock.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+class Clock extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date()
+    }
+    this.tick = this.tick.bind(this);
+  }
+
+  tick() {
+    // debugger
+    this.setState({time: new Date()});
+  }
+
+  componentDidMount() {
+    // debugger
+    this.handle = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.handle);
+  }
+
+  render() {
+    let hours = this.state.time.getHours();
+    let minutes = this.state.time.getMinutes();
+    let seconds = this.state.time.getSeconds();
+    
+    return(
+      <div className="container">
+        <h1 id="clock">Clock</h1>
+          <div className="time">
+            <h1>Time:</h1>
+            <span> {hours}:{minutes}:{seconds} </span>
+          </div>
+      </div>
+      
+    )
+  }
+}
+
+export default Clock;
--- Week_Ten/D2/widgets/frontend/widgets.jsx
+++ Week_Ten/D2/widgets/frontend/widgets.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Clock from "./clock"; // maybe come back here to add "./"
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>it werk! /o/</h1>, root);
+  ReactDOM.render(<Clock />, root);
 });
