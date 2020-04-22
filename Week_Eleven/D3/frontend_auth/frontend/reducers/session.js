6781425a53c96b79086d62e62d3899b05548e77d
Shaphen <swpburn@gmail.com>
2020-04-22 11:59:15 -0700

    complete signup auth and redirect to chirps page

--- Week_Eleven/D3/frontend_auth/frontend/reducers/session.js
+++ Week_Eleven/D3/frontend_auth/frontend/reducers/session.js
@@ -2,9 +2,9 @@ import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session'
 
 const _nullSession = {
   currentUser: null
-},
+};
 
-export default(state = _nullSession, action) => {
+export default (state = _nullSession, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
@@ -12,6 +12,6 @@ export default(state = _nullSession, action) => {
     case LOGOUT_CURRENT_USER:
       return _nullSession;
     default:
-      state;
+      return state;
   }
-}
+};
