79bd1a425d1bb378ef9d05b8a1c183d93cd4c34f
Shaphen <swpburn@gmail.com>
2020-05-18 15:27:37 -0700

    Complete memoization

--- Week_Fifteen/D1/memoization_project/lib/leet_code_518.js
+++ Week_Fifteen/D1/memoization_project/lib/leet_code_518.js
@@ -1,3 +1,17 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+function change(amt, coins, memo={}) {
+  let key = amt + '-' + coins;
+  if (key in memo) return memo[key];
+  if (amt === 0) return 1;
+
+  let currCoin = coins[coins.length-1];
+  let total = 0;
+  for (let i = 0; i * currCoin <= amt; i++) {
+    total += change(amt - i*currCoin, coins.slice(0, -1), memo);
+  }
+
+  memo[key] = total;
+  return memo[key];
+}
